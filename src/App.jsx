25258959d75129209c93ca5e02680757d3ab67e3
antonredko <antonredko11116@gmail.com>
2021-05-06 16:49:32 +0300

    start with card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import PostsList from "./components/PostsList/PostsList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+
 function App() {
   
   return (
--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,16 +1,25 @@
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
 
 export default function PostItem({item, users, comments}) {
 
     return (
+        // <li className="post">
+        //     <header>
+        //         <h2 className="post_author">{item.user?.name}</h2>
+        //     </header>
+        //     <h3 className="post_title">{item.title}</h3>
+        //     <p className="post_text">{item.body}</p>
+        //     <footer>
+        //         <p className="post_comments">Comments: {item.comments?.length}</p>
+        //     </footer>
+        // </li>
         <li className="post">
-            <header>
-                <h2 className="post_author">{item.user?.name}</h2>
-            </header>
-            <h3 className="post_title">{item.title}</h3>
-            <p className="post_text">{item.body}</p>
-            <footer>
-                <p className="post_comments">Comments: {item.comments?.length}</p>
-            </footer>
+        <Card title={item.user?.name}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+            <p>Comments: {item.comments?.length}</p>
+        </Card>
         </li>
     )
 }
