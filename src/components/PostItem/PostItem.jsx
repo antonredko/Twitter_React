a1adf66933f577d454f18258978a3f2d6feb8abb
Anton Redko <antonredko11116@gmail.com>
2021-05-07 19:42:06 +0300

    card antd

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,73 +1,73 @@
-import { List, Avatar, Space } from 'antd';
+import { Card } from 'antd';
 import { EditOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons';
 import React from 'react'
 
-// const { Meta } = Card
+const { Meta } = Card
 
 export default function PostItem({item, users, comments}) {
 
-  const listData = {
-    title: `<h2>${item.user?.name}</h2>`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: `${item.title}`,
-    content: `${item.body}`
-  }
-
-  const IconText = ({ icon, text }) => (
-    <Space>
-      {React.createElement(icon)}
-      {text}
-    </Space>
-  )
+//   const listData = {
+//     title: `${item.user?.name}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description: `${item.title}`,
+//     content: `${item.body}`
+//   }
+  
+//   const IconText = ({ icon, text }) => (
+//     <Space>
+//       {React.createElement(icon)}
+//       {text}
+//     </Space>
+//   )
 
   return (
 
-    // <li className="post">
-    //   <Card
-    //     title={<h2>{item.user?.name}</h2>}
-    //     actions={[
-    //       <LikeOutlined />,
-    //       <EditOutlined />,
-    //       <MessageOutlined />
-    //     ]}
-    //   >
-    //     <Meta
-    //       title={item.title}
-    //       description={item.body}
-    //     />
-    //   </Card>
-    // </li>
+    <li className="post">
+      <Card
+        title={<h2>{item.user?.name}</h2>}
+        actions={[
+          <LikeOutlined style={{ fontSize: '18px' }} />,
+          <EditOutlined style={{ fontSize: '18px' }} />,
+          <MessageOutlined style={{ fontSize: '18px' }} />
+        ]}
+      >
+        <Meta
+          title={item.title}
+          description={item.body}
+        />
+      </Card>
+    </li>
 
 
-    <List
-      itemLayout="vertical"
-      size="large"
-      dataSource={listData}
-      renderItem={item => (
-        <List.Item
-          // key={item.title}
-          actions={[
-            <IconText icon={LikeOutlined} text="" key="list-vertical-star-o" />,
-            <IconText icon={EditOutlined} text="" key="list-vertical-like-o" />,
-            <IconText icon={MessageOutlined} text={item.comments?.length} key="list-vertical-message" />,
-          ]}
-          // extra={
-          //   <img
-          //     width={272}
-          //     alt="logo"
-          //     src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-          //   />
-          // }
-        >
-          <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
-            description={item.description}
-          />
-            {item.content}
-          </List.Item>
-      )}
-    />
+    // <List
+    //   itemLayout="vertical"
+    //   size="large"
+    //   dataSource={listData}
+    //   renderItem={item => (
+    //     <List.Item
+    //       key={item.id}
+    //       actions={[
+    //         <IconText icon={LikeOutlined} text="" key="list-vertical-star-o" />,
+    //         <IconText icon={EditOutlined} text="" key="list-vertical-like-o" />,
+    //         <IconText icon={MessageOutlined} text={item.comments?.length} key="list-vertical-message" />,
+    //       ]}
+    //       extra={
+    //         <img
+    //           width={272}
+    //           alt="logo"
+    //           src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+    //         />
+    //       }
+    //     >
+    //       <List.Item.Meta
+    //         avatar={<Avatar src={item.avatar} />}
+    //         title={<a href={item.href}>{item.title}</a>}
+    //         description={item.description}
+    //       />
+    //         {item.content}
+    //       </List.Item>
+    //   )}
+    // />
 
   )
 }
