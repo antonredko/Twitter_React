cf12b39b78c068c2f0cd082d8d4726ad3ae13cdd
antonredko <antonredko11116@gmail.com>
2021-05-07 17:36:13 +0300

    failed render of list with antd

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,25 +1,73 @@
-import 'antd/dist/antd.css';
-import { Card } from 'antd';
+import { List, Avatar, Space } from 'antd';
+import { EditOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons';
+import React from 'react'
+
+// const { Meta } = Card
 
 export default function PostItem({item, users, comments}) {
 
-    return (
-        // <li className="post">
-        //     <header>
-        //         <h2 className="post_author">{item.user?.name}</h2>
-        //     </header>
-        //     <h3 className="post_title">{item.title}</h3>
-        //     <p className="post_text">{item.body}</p>
-        //     <footer>
-        //         <p className="post_comments">Comments: {item.comments?.length}</p>
-        //     </footer>
-        // </li>
-        <li className="post">
-        <Card title={item.user?.name}>
-            <h3>{item.title}</h3>
-            <p>{item.body}</p>
-            <p>Comments: {item.comments?.length}</p>
-        </Card>
-        </li>
-    )
+  const listData = {
+    title: `<h2>${item.user?.name}</h2>`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description: `${item.title}`,
+    content: `${item.body}`
+  }
+
+  const IconText = ({ icon, text }) => (
+    <Space>
+      {React.createElement(icon)}
+      {text}
+    </Space>
+  )
+
+  return (
+
+    // <li className="post">
+    //   <Card
+    //     title={<h2>{item.user?.name}</h2>}
+    //     actions={[
+    //       <LikeOutlined />,
+    //       <EditOutlined />,
+    //       <MessageOutlined />
+    //     ]}
+    //   >
+    //     <Meta
+    //       title={item.title}
+    //       description={item.body}
+    //     />
+    //   </Card>
+    // </li>
+
+
+    <List
+      itemLayout="vertical"
+      size="large"
+      dataSource={listData}
+      renderItem={item => (
+        <List.Item
+          // key={item.title}
+          actions={[
+            <IconText icon={LikeOutlined} text="" key="list-vertical-star-o" />,
+            <IconText icon={EditOutlined} text="" key="list-vertical-like-o" />,
+            <IconText icon={MessageOutlined} text={item.comments?.length} key="list-vertical-message" />,
+          ]}
+          // extra={
+          //   <img
+          //     width={272}
+          //     alt="logo"
+          //     src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+          //   />
+          // }
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={item.avatar} />}
+            title={<a href={item.href}>{item.title}</a>}
+            description={item.description}
+          />
+            {item.content}
+          </List.Item>
+      )}
+    />
+
+  )
 }
