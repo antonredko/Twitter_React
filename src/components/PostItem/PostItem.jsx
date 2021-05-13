0bb01b0f7ea9535dcae62bccceee9da182e69560
Anton Redko <antonredko11116@gmail.com>
2021-05-14 02:02:51 +0300

    SOME

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd';
+import { Card, Space } from 'antd';
 import { EditOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons';
 import React from 'react'
 
@@ -6,29 +6,18 @@ const { Meta } = Card
 
 export default function PostItem({item, users, comments}) {
 
-//   const listData = {
-//     title: `${item.user?.name}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description: `${item.title}`,
-//     content: `${item.body}`
-//   }
-  
-//   const IconText = ({ icon, text }) => (
-//     <Space>
-//       {React.createElement(icon)}
-//       {text}
-//     </Space>
-//   )
-
   return (
 
     <li className="post">
       <Card
         title={<h2>{item.user?.name}</h2>}
         actions={[
-          <LikeOutlined style={{ fontSize: '18px' }} />,
-          <EditOutlined style={{ fontSize: '18px' }} />,
-          <MessageOutlined style={{ fontSize: '18px' }} />
+            <LikeOutlined />,
+            <EditOutlined />,
+            <CardIcon
+                icon={ <MessageOutlined /> }
+                text={ item.comments?.length }
+            />
         ]}
       >
         <Meta
@@ -38,36 +27,16 @@ export default function PostItem({item, users, comments}) {
       </Card>
     </li>
 
-
-    // <List
-    //   itemLayout="vertical"
-    //   size="large"
-    //   dataSource={listData}
-    //   renderItem={item => (
-    //     <List.Item
-    //       key={item.id}
-    //       actions={[
-    //         <IconText icon={LikeOutlined} text="" key="list-vertical-star-o" />,
-    //         <IconText icon={EditOutlined} text="" key="list-vertical-like-o" />,
-    //         <IconText icon={MessageOutlined} text={item.comments?.length} key="list-vertical-message" />,
-    //       ]}
-    //       extra={
-    //         <img
-    //           width={272}
-    //           alt="logo"
-    //           src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-    //         />
-    //       }
-    //     >
-    //       <List.Item.Meta
-    //         avatar={<Avatar src={item.avatar} />}
-    //         title={<a href={item.href}>{item.title}</a>}
-    //         description={item.description}
-    //       />
-    //         {item.content}
-    //       </List.Item>
-    //   )}
-    // />
-
   )
+}
+
+function CardIcon({icon, text}) {
+    return (
+        <>
+            <Space>
+                {icon}
+                {text}
+            </Space>
+        </>
+    )
 }
